8f3426ba25840abe81327bc85c83473c3628ffa0
David Safronsky <safronsdavid777@gmail.com>
2024-07-22 00:45:10 -0400

    2nd Commit

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
+from random import choices
+
+
 class Game:
     def __init__(self, num_players):
         self.deck = [Card(v, s) for s in ["Hearts", "Clubs", "Diamonds", "Spades"]
                      for v in ["Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King"]]
-        self.num_players = num_players
-        player = Hand(Card("Ace", "Hearts"), Card("2", "Clubs"))
-        print(player)
+        self.hands = [Hand() for _ in range(num_players)]
+        self.deal()
+
+    def deal(self):
+        for hand in self.hands:
+            cards = choices(self.deck, k=2)
+            hand.add(cards[0], cards[1])
+            self.deck = [c for c in self.deck if c not in cards]
 
 
 class Card:
@@ -31,13 +39,14 @@ class Hand:
 
         return ret
 
-
-    def add(self, card):
+    def add(self, c1, c2=None):
         if len(self.cards) == 2:
             raise Exception("Hand is full")
+        if len(self.cards) == 1 and c2:
+            raise Exception("1 Card in Hand: Adding Too Many Cards")
 
-        self.cards.append(card)
-
+        self.cards.append(c1)
+        if c2: self.cards.append(c2)
 
 
 if __name__ == "__main__":
